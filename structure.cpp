a57a7481692d17c64a79c7cbde8fe957b429fa68
Faye Kenoua Diop <kenouadiop@gmail.com>
2025-12-15 23:44:50 +0100

    Feat: lists in c++ Scrivere un codice che presenti una struttura nodo di una lista con i suoi eventuali metodi (inserimento/cancellazione in testa, coda, di un valore preciso (presenta anche un metodo per l'inserimento ordinato in caso di lista ordinata)).

--- structure.cpp
+++ structure.cpp
@@ -1,50 +1,58 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
-struct punto{
+struct PUNTO{
     int x = 0;
     int y = 0;
     };
 
-void disegnaGrafico(punto funzione[], int dim);
+void disegnaGrafico(PUNTO funzione[], int dim);
+double distanzaPunti(PUNTO *a, PUNTO b){
+    double d;
+    d = sqrt(pow(((double)(b.y - (*a).y)), 2) + pow(((double)(b.x - (*a).x)), 2));
+    return d;
+}
 
 int main(){
 
-    punto a;
-    punto b;
+    PUNTO a;
+    PUNTO b;
 
-    cout << "Inserisci le coordinate del primo punto (x1, y1): ";
+    cout << "Inserisci le coordinate del primo PUNTO (x1, y1): ";
     cin >> a.x >> a.y;
 
-    cout << "Inserisci le coordinate del secondo punto (x2, y2): ";
+    cout << "Inserisci le coordinate del secondo PUNTO (x2, y2): ";
     cin >> b.x >> b.y;
 
-    int dx = b.x - a.x;
-    int dy = b.y - a.y;
+    PUNTO *pa = &a;
+    PUNTO *pb = &b;
 
-    double distanza = (float)(b.y - a.y) / (float)(b.x - a.x);
+    double distanza = distanzaPunti(pa, *pb);
 
     cout << "La distanza tra i due punti e': " << distanza << endl;
 
 
     //funzione
     // y = mx + q = 2x   I = [0, 99]
-    punto funzione[100];
-    for (int i = 0; i < 100; i++) {
-        funzione[i].x = i;
-        funzione[i].y = 2 * i;
-    }
-
-    disegnaGrafico(funzione, 100);
+    // int m, q;
+    // cout << "y = mx + q \n inserisci la m e la q: (F: I -> [0, 100])" << endl;
+    // cin >> m >> q;
+    // PUNTO funzione[101];
+    // for (int i = 0; i <= 100; i++) {
+    //     funzione[i].x = i;
+    //     funzione[i].y = (m * i) + q;
+    // }
+    // disegnaGrafico(funzione, 100);
 }
 
-void disegnaGrafico(punto funzione[], int dim){ //non efficiente, ma semplice
+void disegnaGrafico(PUNTO funzione[], int dim){ //non efficiente, ma semplice
     cout << "Grafico della funzione: " << endl;
     for (int i = dim-1; i > 0; i--) {
         for(int j = 0; j < funzione[i].x; j++) {
             cout << " ";
         }
-        cout << "*"; // punto della funzione
+        cout << "*"; // PUNTO della funzione
         for(int j = 0; j < (funzione[i].y-funzione[i-1].y); j++) {
            cout << endl;;
         }
@@ -53,6 +61,6 @@ void disegnaGrafico(punto funzione[], int dim){ //non efficiente, ma semplice
     for(int j = 0; j < funzione[0].x; j++) {
         cout << " ";
     }
-    cout << "*"; // punto della funzione
+    cout << "*"; // PUNTO della funzione
     cout << endl;
 }
